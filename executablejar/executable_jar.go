5e27ebf8ac5b686461ea4fa9dc3037559485d9dd
Ben Hale <bhale@pivotal.io>
2019-06-11 08:09:46 -0400

    Copyright Updates
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- executablejar/executable_jar.go
+++ executablejar/executable_jar.go
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -40,7 +40,7 @@ type ExecutableJAR struct {
 	layers layers.Layers
 }
 
-// Contribute makes the contribution to launch
+// Contribute makes the contribution to launch.
 func (e ExecutableJAR) Contribute() error {
 	if err := e.layer.Contribute(e.Metadata, func(layer layers.Layer) error {
 		return layer.AppendPathSharedEnv("CLASSPATH", strings.Join(e.Metadata.ClassPath, string(filepath.ListSeparator)))
